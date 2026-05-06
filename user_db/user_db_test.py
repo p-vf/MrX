27a5cc9f66ae1cbbd92c6e8c765881433b5cdd6e
Alexander Marggraf <a.marggraf@stud.unibas.ch>
2026-05-06 19:31:08 +0200

    Add Database to store users, roles and password

---
+++ user_db/user_db_test.py
@@ -0,0 +1,76 @@
+import sqlite3
+from user_db import UserDBManager
+
+NO_OF_TESTS = 6
+
+class UserDBTest:
+    def __init__(self):
+        self.db = UserDBManager("user.db")
+        self.db.nuke_db()
+
+    def test_insert(self):
+        username_1 = "chud"
+        role_1 = 0
+        password_1 = "chopped_1"
+
+        username_2 = "chad"
+        role_2 = 1
+        password_2 = "mogging-2026"
+
+        self.db.insert_user(username_1, role_1, password_1)
+        self.db.insert_user(username_2, role_2, password_2)
+
+    def test_get(self):
+        chud = self.db.get_user("chud")
+        chad = self.db.get_user("chad")
+
+        assert chud[0] == "chud"
+        assert chud[1] == 0
+
+        assert chad[0] == "chad"
+        assert chad[1] == 1
+
+    def test_change_username(self):
+        self.db.change_username("chud", "nothingeverhappens")
+
+        chud = self.db.get_user("chud")
+        neh = self.db.get_user("nothingeverhappens")
+
+        assert chud == None
+        assert neh != None
+
+    def test_change_password(self):
+        old_chad = self.db.get_user("chad")
+        self.db.change_password("chad", "based")
+        new_chad = self.db.get_user("chad")
+        
+        assert old_chad[2] != new_chad[2]
+        assert old_chad[3] != new_chad[3]
+
+    def test_change_role(self):
+        self.db.change_role("chad", 0)
+        chad = self.db.get_user("chad")
+
+        assert chad[1] == 0
+    
+    def test_delete(self):
+        self.db.delete_user("chud")
+        self.db.delete_user("chad")
+
+        chud = self.db.get_user("chud")
+        chad = self.db.get_user("chad")
+
+        assert chud == None
+        assert chad == None
+
+if __name__ == "__main__":
+    test = UserDBTest()
+
+    test.test_insert()
+    test.test_get()
+    test.test_change_username()
+    test.test_change_password()
+    test.test_change_role()
+    test.test_delete()
+
+    print(f"passed all {NO_OF_TESTS} tests")
