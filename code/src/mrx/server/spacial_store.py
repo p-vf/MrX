0f29a39ec84d2010bab92d7a333c629577139d49
Pascal von Fellenberg <pvonfellenberg@bluewin.ch>
2026-06-10 00:12:11 +0200

    implement the calculation of region sizes in SpacialStore

--- code/src/mrx/server/spacial_store.py
+++ code/src/mrx/server/spacial_store.py
@@ -1,6 +1,8 @@
 from logic.geometry import get_quadrant, Rect
+from math import sin, pi
 
 class SpacialStore:
+    """convention: x =^= longitude; y =^= latitude"""
     def __init__(self, startrect: Rect):
         self.startrect = startrect
         self._user_path: dict[str, list[int]] = {}
@@ -22,15 +24,36 @@ class SpacialStore:
     def get_path(self, user_id: str) -> list[int]:
         return self._user_path[user_id]
 
+    # https://www.johndcook.com/blog/2023/02/21/sphere-grid-area/
+    def get_accuracy_per_depth(self, num_accuracies) -> list[float]:
+        """returns a list, where the i-th element corresponds to an estimate*
+        of the area of regions at depth i. The unit of this list is km^2. <br>
+        *: this is only an estimate, because regions defined by this
+        partitioning at a certain depth can have different areas depending on
+        their latitude; A latitude-longitude "rectangle" at the north-pole for
+        example has a smaller area than the same "rectangle" at the equator.
+        """
+        r = 6_371 # radius of the earth in km
+        total_area = \
+               r**2 \
+            * (sin(self.startrect.y_max / 180 * pi) - sin(self.startrect.y_min / 180 * pi)) \
+            * (self.startrect.x_max - self.startrect.x_min) / 180 * pi
+        accuracy_list = []
+        curr_area = total_area
+        for _ in range(num_accuracies):
+            accuracy_list.append(curr_area)
+            curr_area *= 0.25
+        return accuracy_list
+
     def get_area(self, user_id: str, accuracy: int = -1) -> Rect:
         """
         an accuracy of 0 means the lowest possible accuracy (the start rect is returned).
-        if accuracy is less than 0, we return the rectangle with the maximum
+        if accuracy is less than 0, we return the rectangle with the maximuma
         accuracy.
         """
-        curr_rect = self.startrect
-        if accuracy == 0:
+        if accuracy < 0:
             return self.startrect
+        curr_rect = self.startrect
         path = self._user_path[user_id][:accuracy]
         for choice in path:
             curr_rect = get_quadrant(curr_rect, choice)
